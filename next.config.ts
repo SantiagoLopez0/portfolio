f4b5709b5860e526b01627ab1e2882ad93c163d9
Santiago <asantiago0034@gmail.com>
2024-12-14 01:07:55 -0500

    Next.js config - config to publish 7.0

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  basePath: '/santiagolopez0.github.io',
+  assetPrefix: '/santiagolopez0.github.io/',
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
