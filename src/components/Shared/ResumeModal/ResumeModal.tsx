81c7d076749fed2d669824b85ba36b00d756331e
Santiago <asantiago0034@gmail.com>
2024-12-29 18:16:18 -0500

    Updating resume modal. Adding PDF

--- src/components/Shared/ResumeModal/ResumeModal.tsx
+++ src/components/Shared/ResumeModal/ResumeModal.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+// import Image from 'next/image';
 import Modal from 'react-modal'; // Importing react-modal
 import { useState } from 'react';
 
 import styles from './resumeModal.module.css';
 
-export const ResumeModal = ({ isButton }: { isButton: boolean }) => {
+export const ResumeModal = (
+    { 
+        isButton,
+    }:{ 
+        isButton: boolean 
+    }) => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
     const openModal = () => setModalIsOpen(true);
@@ -19,18 +24,16 @@ export const ResumeModal = ({ isButton }: { isButton: boolean }) => {
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
                 contentLabel="Resume Modal"
-                className={`${styles.resumeModal} max-w-[100%] md:h-[95vh] md:w-[auto] flex items-center overflow-auto`}
+                className={`${styles.resumeModal} max-w-[100%] w-[90vw] md:w-[70%] h-[76vh] md:h-[98vh] flex flex-col justify-between items-center overflow-auto`}
                 ariaHideApp={false}
                 overlayClassName={styles.resumeModal__overlay}
             >
-                <button onClick={closeModal} className={styles.resumeModal__closeButton}>X</button>
-                <Image
-                    src="/resume.svg"
-                    alt="Santiago"
-                    className={styles.modal__mainImage}
-                    width={400}
-                    height={900}
-                />
+                <button onClick={closeModal} className={`${styles.resumeModal__closeButton} hidden md:flex`}>X</button>
+                <iframe src="/Resume.pdf#zoom=100" className={`hidden md:block ${styles.modal__file}`} ></iframe>
+                <iframe src="https://drive.google.com/file/d/1R-oRjCwTx6o-EJEBMV4kHcQoJya7OMyP/preview" className={`block md:hidden ${styles.modal__file}`} ></iframe>
+                <div className="flex mt-16 md:hidden block">
+                    <button onClick={closeModal} className={styles.resumeModal__mobileCloseButton}>Close</button>
+                </div>
             </Modal>
         </>
     )
