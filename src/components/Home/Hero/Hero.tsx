f836ef74daa42bf1af101f2fab1f8ad25be8b7cf
Santiago <asantiago0034@gmail.com>
2024-12-29 19:07:13 -0500

    adding multiple titles to header and animation

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -1,23 +1,29 @@
 "use client"
 
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./hero.module.css";
 import { Button } from "app/components/Shared/Button";
 import { ResumeModal } from "app/components/Shared/ResumeModal";
 
 
-const keywords = ["Web", "Webflow", "Frontend"];
+const titles = ["Web", "Webflow", "Frontend"];
 
 export const Hero = () => {
-    // const [currentKeyword, setCurrentKeyword] = useState(0);
+    const [index, setIndex] = useState(0);
+    const [fade, setFade] = useState(false);
 
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setCurrentKeyword((prevKeyword) => (prevKeyword + 1) % keywords.length);
-    //     }, 3000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFade(true);
+            setTimeout(() => {
+                setIndex((prevIndex) => (prevIndex + 1) % titles.length);
+                setFade(false);
+            }, 500); // Half the duration for smoother transition
+        }, 3000);
 
-    //     return () => clearInterval(interval);
-    // }, []);
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <section className="md:h-[100vh] flex items-center md:pt-[128px] pb-64 md:pb-0">
@@ -26,8 +32,11 @@ export const Hero = () => {
                     {/* Left Section */}
                     <div className="pt-[48px] md:pt-0 w-full h-[100vh] md:h-auto md:max-w-[60%] flex flex-col justify-center items-start gap-[10px]">
                         <h1 className={`${styles.mainTitle}`}>
-                            {/* <span className={styles.animatedKeyword}>{keywords[currentKeyword]}</span><br /> Developer */}
-                            {keywords[0]} Developer
+                            <span className={`inline-block transition-opacity duration-500 ${fade ? 'opacity-0' : 'opacity-100'}`}>
+                                {titles[index]}
+                            </span>
+                            <br />
+                            Developer
                         </h1>
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-32">
                             <p className="text-left">
@@ -41,11 +50,9 @@ export const Hero = () => {
                         </div>
 
                         <div className="md:h-[90px] flex flex-col md:flex-row mt-16 md:mt-32 gap-32">
-                            {/* <Button href='https://drive.google.com/file/d/1R-oRjCwTx6o-EJEBMV4kHcQoJya7OMyP/view?usp=sharing' text='View my resume' /> */}
-                            <ResumeModal isButton={true} customClassName=""/>
-                            <Button href='https://www.linkedin.com/in/santiago-lopez-b18594193/' text='LinkedIn' theme="dark"/>
+                            <ResumeModal isButton={true} customClassName="" />
+                            <Button href='https://www.linkedin.com/in/santiago-lopez-b18594193/' text='LinkedIn' theme="dark" />
                         </div>
-
                     </div>
 
                     {/* Right Section */}
